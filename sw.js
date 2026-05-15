330b6ec32545b8fa14e463f0bf977859d8b75b71
khmin23 <55825yzrhc@privaterelay.appleid.com>
2026-05-15 14:44:30 +0900

    Deploy Namakjip interior viewer

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'miri-hankki-v29-situation-recs'
+const CACHE_NAME = 'miri-hankki-v29-namakjip-interior'
 const BASE_PATH = new URL(self.registration.scope).pathname
 const APP_SHELL = [
   BASE_PATH,
