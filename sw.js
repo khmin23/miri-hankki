de0cd7076f74a06c4b5f40248339f8b16f80c3dc
khmin23 <55825yzrhc@privaterelay.appleid.com>
2026-05-17 01:38:15 +0900

    Deploy interactive Leaflet restaurant map

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'miri-hankki-v29-map-first-home'
+const CACHE_NAME = 'miri-hankki-v30-leaflet-map'
 const BASE_PATH = new URL(self.registration.scope).pathname
 const APP_SHELL = [
   BASE_PATH,
