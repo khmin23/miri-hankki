4c03d6914ae5696222f7bd510c24207c5ecf3520
김효민 <kimhyomin@gimhyomin-ui-MacBookPro.local>
2026-05-30 18:48:57 +0900

    Deploy code cleanup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'miri-hankki-v1780133948426'
+const CACHE_NAME = 'miri-hankki-v1780134517641'
 const BASE_PATH = new URL(self.registration.scope).pathname
 
 // Vite가 생성한 해시 기반 asset만 캐시 우선 — 나머지는 항상 네트워크 우선
